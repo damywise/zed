2e67b6a7eb3627a4e4aed6a5bd98f0a01a904887
Joseph T. Lyons <JosephTLyons@gmail.com>
2025-07-16 04:06:36 -0400

    Make less generic

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -12142,9 +12142,9 @@ impl Editor {
         };
 
         window.dispatch_action(
-            Box::new(DiffText {
-                old_text_source: TextSource::Clipboard(clipboard_text),
-                new_text_source: TextSource::Editor(cx.entity()),
+            Box::new(DiffClipboardWithSelectionData {
+                clipboard_text,
+                editor: cx.entity(),
             }),
             cx,
         );
